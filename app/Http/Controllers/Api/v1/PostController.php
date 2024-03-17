44f1de2cf725178d9e9ba08a9a0d8168e618da7c
nikronsakha <nikitborisov15@gmail.com.com>
2024-03-17 11:14:01 +0600

    add migrate and api route

---
+++ app/Http/Controllers/Api/v1/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+
+    }
+    public function show()
+    {
+
+    }
+    public function store()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function delete()
+    {
+
+    }
+}
