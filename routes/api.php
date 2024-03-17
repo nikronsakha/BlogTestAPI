d334bf5b8ee2a6352a257b9558cb07c3716be17f
nikronsakha <nikitborisov15@gmail.com.com>
2024-03-17 12:20:35 +0600

    add api v1

--- routes/api.php
+++ routes/api.php
@@ -3,26 +3,26 @@
 use App\Http\Controllers\Api\v1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//
-//Route::prefix('v1')
-//    ->name('v1')
-//    ->group(function (){
-//        Route::get('/posts' , PostController::class, 'index')
-//            ->name('posts.index');
-//
-//
-//        Route::get('/posts/{post}' , PostController::class, 'show')
-//            ->name('posts.show');
-//
-//
-//        Route::post('/posts' , PostController::class, 'store')
-//            ->name('posts.store');
-//
-//
-//        Route::put('/posts/{post}' , PostController::class, 'update')
-//            ->name('posts.update');
-//
-//
-//        Route::delete('/posts/{post}' , PostController::class, 'destroy')
-//            ->name('posts.delete');
-//    });
+
+Route::prefix('v1')
+    ->name('v1')
+    ->group(function (){
+        Route::get('/posts' , [PostController::class, 'index'])
+            ->name('posts.index');
+
+
+        Route::get('/posts/{post}' , [PostController::class, 'show'])
+            ->name('posts.show');
+
+
+        Route::post('/posts' , [PostController::class, 'store'])
+            ->name('posts.store');
+
+
+        Route::put('/posts/{post}' , [PostController::class, 'update'])
+            ->name('posts.update');
+
+
+        Route::delete('/posts/{post}' , [PostController::class, 'destroy'])
+            ->name('posts.delete');
+    });
